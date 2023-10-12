34eda5dc674ad1abddb977938d7ef43b2adaf806
Tqqn <mailtoonie@gmail.com>
2023-10-12 15:44:43 +0200

    Initial commit

---
+++ src/main/java/com/tqqn/lobbyhalloween/LightningSpawnCommand.java
@@ -0,0 +1,2 @@
+package com.tqqn.lobbyhalloween;public class DebugCommand {
+}
---
+++ src/main/java/com/tqqn/lobbyhalloween/LobbyHalloween.java
@@ -0,0 +1,17 @@
+package com.tqqn.lobbyhalloween;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LobbyHalloween extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
