d3c3d5b5f749d5ba2dcfd12d66ebd01af102e0d8
Tqqn <mailtoonie@gmail.com>
2023-10-12 23:27:05 +0200

    edit README.md

--- README.md
+++ README.md
@@ -3,10 +3,13 @@ MC-Version: 1.16.4 (Wont work on other versions because of NMS.)
 ## Description:
 - This is a fun plugin that will randomly spawn lightning on the locations that are stated in the config.
   Lightning will spawn some spooky bats that will fly around and disappear after a while.
+- Enderman jumpscare that will scare a player.
 
 ## Commands:
 ### Admin Commands:
-- /spawnlightning <player> - Spawns lightning on the players location.
+- /spawnlightning <player/name> - Spawns lightning on the players location.
+- /lightning <true/false> - Toggle off/on the lightning spawn.
+- /scare <player/name> - Jumpscares the player with an enderman.
 
 ### Disclaimer:
 This plugin is build for private use/for some own funs. 
--- src/main/java/com/tqqn/lobbyhalloween/commands/ScareCommand.java
+++ src/main/java/com/tqqn/lobbyhalloween/commands/ScareCommand.java
@@ -1,6 +1,8 @@
 package com.tqqn.lobbyhalloween.commands;
 
 import com.tqqn.lobbyhalloween.CustomScare;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -13,9 +15,13 @@ public class ScareCommand implements CommandExecutor {
         if (!(sender instanceof Player)) return false;
         Player player = (Player) sender;
 
-        CustomScare customScare = new CustomScare(player);
-        customScare.scare();
-
+        if (args.length < 1 || Bukkit.getPlayer(args[0]) == null) {
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cThis player is not online!"));
+            return false;
+        } else {
+            CustomScare customScare = new CustomScare(Bukkit.getPlayer(args[0]));
+            customScare.scare();
+        }
         return true;
     }
 }
